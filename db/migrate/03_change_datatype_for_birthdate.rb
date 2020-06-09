0a75728f4e9fed2ee35517b69a4b9896ca9c536c
PARDEEP KAUR <pkaur84@aol.com>
2020-06-09 19:27:43 +0000

    Done.

--- db/migrate/03_change_datatype_for_birthdate.rb
+++
@@ -1,8 +0,0 @@
-class ChangeDatatypeForBirthdate < ActiveRecord::Migration[5.1]
-    def Change
-      change_column :students, :birthdate, :integer
-      change_column :students, :grade, :integer
-    
-      end 
-  end
-    
