a265420cb719d7ef829288ee3f812cf9e773ecd0
PARDEEP KAUR <pkaur84@aol.com>
2020-06-10 03:58:22 +0000

    Done.

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,8 +1,4 @@
 class Student < ActiveRecord::Base
-  # attr_accessor
-    # initialize
-    # save
-    # find_by
-    # all 
+ 
 end
 
--- db/migrate/02_add_grade_and_birthdate_to_students.rb
+++ db/migrate/02_add_grade_and_birthdate_to_students.rb
@@ -1,4 +1,4 @@
-class CreateStudents < ActiveRecord::Migration[5.2]
+class AddGradeAndBirthdateToStudents < ActiveRecord::Migration[5.2]
   
   def change
     add_coulmn :students , :grade , :Integer
